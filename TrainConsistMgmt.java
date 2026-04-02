1f5fd9325ade9163f4b729fc05bbe014b7137ee1
Wild-Dandelion <sm5756@srmist.edu.in>
2026-04-02 09:07:15 +0530

    UC 10: Count Total Seats in Train

--- TrainConsistMgmt.java
+++ TrainConsistMgmt.java
@@ -1,3 +1,63 @@
-void main() {
-    
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    void display() {
+        System.out.println(name + " -> " + capacity);
+    }
+}
+
+public class TrainConsistMgmt {
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println("UC10 Count Total Seats in Train");
+        System.out.println("================================\n");
+
+        List<Bogie> bogies = new ArrayList<>();
+
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+        bogies.add(new Bogie("First Class", 24));
+        bogies.add(new Bogie("Sleeper", 70));
+        bogies.add(new Bogie("AC Chair", 60));
+
+        System.out.println("All Bogies:");
+        bogies.forEach(b -> b.display());
+
+
+        List<Integer> capacities = bogies.stream()
+                .map(b -> b.capacity)
+                .collect(Collectors.toList());
+
+
+        int totalCapacity = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
+
+        System.out.println("\nTotal Seating Capacity: " + totalCapacity);
+
+
+        Map<String, List<Bogie>> groupedBogies = bogies.stream()
+                .collect(Collectors.groupingBy(b -> b.name));
+
+        System.out.println("\nGrouped Bogies:");
+
+        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
+            System.out.println("Bogie Type: " + entry.getKey());
+
+            for (Bogie b : entry.getValue()) {
+                System.out.println("Capacity -> " + b.capacity);
+            }
+        }
+        System.out.println("\nUC10 aggregation completed...");
+    }
 }
